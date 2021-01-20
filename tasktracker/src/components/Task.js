633a821aeae382ea397d0cb5311de5df447570a0
Saffat Aziz <saziz028@uottawa.ca>
2021-01-21 02:22:54 +0600

    e

--- tasktracker/src/components/Task.js
+++ tasktracker/src/components/Task.js
@@ -3,7 +3,7 @@ import {FaTimesCircle, FaRegListAlt,FaClock} from 'react-icons/fa'
 const Task = (props) => {
     return (
         <div className = 'task'>
-            <h3><FaRegListAlt/>{props.task.text}<FaTimesCircle style={{color:'red'}} onClick = {props.delete}/> </h3>
+            <h3><FaRegListAlt/>{props.task.text}<FaTimesCircle style={{color:'red'}} onClick = { () => props.delete(props.task.id)}/> </h3>
             <p><FaClock style={{paddingRight:"100px", }}/>{props.task.day}</p>
             
         </div>
