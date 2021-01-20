3c4788ec415d6259ab62b0aba7461c73bf8a022a
Saffat Aziz <saziz028@uottawa.ca>
2021-01-20 06:00:18 +0600

    e

--- tasktracker/src/components/Header.js
+++ tasktracker/src/components/Header.js
@@ -1,13 +1,23 @@
 import Button from './Button'
-const Header = () => {
+import PropTypes from 'prop-types'
+const Header = (props) => {
     return (
         <header className = "header">
             <h1>
-                Task Tracker
+                {props.title}
             </h1>
             <Button/>
         </header>
     )
 }
+Header.defultProps = {
+    title: 'Task Tracker',
+}
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired
+}
+
+
 
 export default Header
