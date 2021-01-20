fc45ef09ea1ec4aff9e3f65c948f549346fccdac
Saffat Aziz <saziz028@uottawa.ca>
2021-01-21 01:21:25 +0600

    Update Tasks.js

--- tasktracker/src/components/Tasks.js
+++ tasktracker/src/components/Tasks.js
@@ -1,6 +1,36 @@
 import React from 'react'
 import { useState } from 'react'
 const Tasks = () => {
+    const [tasks, setTasks] = useState([
+        {
+            id: 1,
+            text: 'Do grocery shopping',
+            day: 'Feb 5th at 2:30pm',
+            reminder: true,
+
+        },
+        {
+            id: 2,
+            text: 'Doctor Appointment',
+            day: 'Feb 7th at 2:30pm',
+            reminder: true,
+
+        },
+        {
+            id: 3,
+            text: 'Do laundry',
+            day: 'Feb 20th at 2:30pm',
+            reminder: true,
+
+        },
+        {
+            id: 4,
+            text: 'Meet with the team',
+            day: 'Feb 11th at 2:30pm',
+            reminder: true,
+
+        },
+    ])
     return (
         <>
             
