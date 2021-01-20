46976e251b389db3d913ae7641690fbded74e42e
Saffat Aziz <saziz028@uottawa.ca>
2021-01-21 01:26:43 +0600

    e

--- tasktracker/src/App.js
+++ tasktracker/src/App.js
@@ -1,6 +1,38 @@
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import { useState } from 'react'
+
 function App() {
+  const [tasks, setTasks] = useState([
+        {
+            id: 1,
+            text: 'Do grocery shopping',
+            day: 'Feb 5th at 2:30pm',
+            reminder: true,
+
+        },
+        {
+            id: 2,
+            text: 'Doctor Appointment',
+            day: 'Feb 7th at 2:30pm',
+            reminder: true,
+
+        },
+        {
+            id: 3,
+            text: 'Do laundry',
+            day: 'Feb 20th at 2:30pm',
+            reminder: true,
+
+        },
+        {
+            id: 4,
+            text: 'Meet with the team',
+            day: 'Feb 11th at 2:30pm',
+            reminder: true,
+
+        },
+    ])
   return (
     <div className = "container" >
       <Header title = "Task Tracker"/>
