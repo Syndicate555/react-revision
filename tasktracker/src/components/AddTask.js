45c87cfb1e672a1bd224cbe5f509213e583fce51
Saffat Aziz <saziz028@uottawa.ca>
2021-01-23 00:03:07 +0600

    changes

--- tasktracker/src/components/AddTask.js
+++ tasktracker/src/components/AddTask.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {useState} from 'react'
 const AddTask = () => {
+    // states
     const [text, setText] = useState('')
     const [day, setDay] = useState('')
     const [reminder, setReminder] = useState(false)
